4138bb2c410209c13c28921d70c58d51da7fc14c
galahadtrail <zzzzrzzzz@yandex.ru>
2024-11-18 15:41:57 +0300

    prints pretty, I'am glad

--- src/output.rs
+++ src/output.rs
@@ -2,10 +2,24 @@ use colored::*;
 use std::{thread, time::Duration};
 
 pub fn print_hello_message() {
-    let mut prnt = "  /$$$$$$$  /$$$$$$  /$$   /$$ /$$   /$$          /$$$$$$$  /$$$$$$   /$$$$$$$"
-        .red()
-        .italic()
-        .on_yellow();
-    thread::sleep(Duration::from_millis(1000));
-    println!("{}", prnt);
+    let binding = "
+                                                                                        
+      /$$$$$$$  /$$$$$$  /$$   /$$ /$$   /$$          /$$$$$$$  /$$$$$$   /$$$$$$$      
+     /$$_____/ /$$__  $$|  $$ /$$/| $$  | $$ /$$$$$$ /$$_____/ /$$__  $$ /$$_____/      
+    |  $$$$$$ | $$$$$$$$ \\  $$$$/ | $$  | $$|______/|  $$$$$$ | $$  \\ $$| $$            
+     \\____  $$| $$_____/  >$$  $$ | $$  | $$         \\____  $$| $$  | $$| $$             
+     /$$$$$$$/|  $$$$$$$ /$$/\\  $$|  $$$$$$$         /$$$$$$$/|  $$$$$$/|  $$$$$$$     
+    |_______/  \\_______/|__/  \\__/ \\____  $$        |_______/  \\______/  \\_______/      
+                                   /$$  | $$                                            
+                                  |  $$$$$$/                                            
+                                  \\______/                                             
+                                                                                        \n"
+    .truecolor(193, 251, 222)
+    .italic();
+    let hello_world_string = binding.split("\n");
+
+    for part in hello_world_string {
+        println!("{}", part.truecolor(193, 251, 222).italic());
+        thread::sleep(Duration::from_millis(200));
+    }
 }
