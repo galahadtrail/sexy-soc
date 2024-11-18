8d275d9f4f6577c94356f0ba4acb252f4deb4623
galahadtrail <zzzzrzzzz@yandex.ru>
2024-11-18 17:23:40 +0300

    some files moved

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,8 @@
-// Copyright (c) 2014, 2015 Robert Clipsham <robert@octarineparrot.com>
-//
-// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
-// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
-// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
-// option. This file may not be copied, modified, or distributed
-// except according to those terms.
-
-/// This example shows a basic packet logger using libpnet
 mod capture;
 mod output;
 mod rules;
 
+use capture::traffic_interception;
 use output::print_hello_message;
 
 mod prelude {
@@ -23,4 +15,5 @@ mod prelude {
 
 fn main() {
     print_hello_message();
+    traffic_interception();
 }
