c203c1c1b11379daa57438fe292aa7567a985040
galahadtrail <zzzzrzzzz@yandex.ru>
2024-11-19 16:41:34 +0300

    removed all trush

--- src/main.rs
+++ src/main.rs
@@ -20,16 +20,4 @@ mod prelude {
 fn main() {
     print_hello_message();
     //traffic_interception();
-    let rls = Rule::new("127.0.0.1".parse().unwrap(), "127.0.0.1".parse().unwrap());
-    let rls1 = Rule::new(
-        "192.168.70.1".parse().unwrap(),
-        "189.23.134.1".parse().unwrap(),
-    );
-    let vec = vec![rls, rls1];
-    //let _ = write_to_file(vec);
-
-    let vct = read_from_file().unwrap();
-    for vc in vct.iter() {
-        println!("{}", vc);
-    }
 }
