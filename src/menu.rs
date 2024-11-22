047487008d0836bfcd0a7b8ee38ba9d4903212ca
galahadtrail <zzzzrzzzz@yandex.ru>
2024-11-22 13:57:29 +0300

    finished for statement machine

--- src/menu.rs
+++ src/menu.rs
@@ -46,7 +46,7 @@ impl TrafficInterception {
     }
     fn to_menu(self) -> Menu {
         Menu {
-            description: String::from("Here you will see network intrusions"),
+            description: String::from("This is menu. You can pick what you want to do here."),
         }
     }
 }
@@ -54,10 +54,50 @@ struct ComputersInformation {
     description: String,
 }
 
+impl ComputersInformation {
+    fn new() -> Self {
+        ComputersInformation {
+            description: String::from("Here you will see agents intrusions"),
+        }
+    }
+    fn to_menu(self) -> Menu {
+        Menu {
+            description: String::from("This is menu. You can pick what you want to do here."),
+        }
+    }
+}
+
 struct NetworkRulesChanging {
     description: String,
 }
 
+impl NetworkRulesChanging {
+    fn new() -> Self {
+        NetworkRulesChanging {
+            description: String::from("Here you change rules for nets"),
+        }
+    }
+
+    fn to_menu(self) -> Menu {
+        Menu {
+            description: String::from("This is menu. You can pick what you want to do here."),
+        }
+    }
+}
 struct ComputerRulesChanging {
     description: String,
+}
+
+impl ComputerRulesChanging {
+    fn new() -> Self {
+        ComputerRulesChanging {
+            description: String::from("Here you change rules for comps"),
+        }
+    }
+
+    fn to_menu(self) -> Menu {
+        Menu {
+            description: String::from("This is menu. You can pick what you want to do here."),
+        }
+    }
 }
